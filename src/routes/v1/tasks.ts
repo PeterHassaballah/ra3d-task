9b453de79e7237de092513b37f11996e04ed4336
PeterHassaballah <peter.hassaballah@gmail.com>
2024-04-07 16:03:55 +0200

    [feat] notifications and assignment routes

--- src/routes/v1/tasks.ts
+++ src/routes/v1/tasks.ts
@@ -1,27 +1,30 @@
 import { Router } from "express";
 import validate from "../../middlewares/validate";
-import { createTask,updateTask } from "../../validations/task";
-import {auth, extractUserId} from "../../middlewares/auth";
+import { createTask, updateTask } from "../../validations/task";
+import { auth, extractUserId } from "../../middlewares/auth";
 import {
   createTask as _createTask,
   getAllTasks as _getTasks,
   getTask as _getTask,
   updateTask as _updateTask,
+  getUserTasks,
   assignTask
-} from "../../controllers/task"; 
+} from "../../controllers/task";
 const router = Router();
 //routes to get/create tasks
 router
   .route("/")
-  .post(auth,validate(createTask),extractUserId, _createTask)
+  .post(auth, validate(createTask), extractUserId, _createTask)
   .get(auth, _getTasks);
-  // assign task
-router.patch("/:taskId/assign",auth,assignTask);
+// get user's tasks
+router.get('/my-tasks', auth, extractUserId, getUserTasks);
+// assign task
+router.patch("/:taskId/assign", auth, assignTask);
 // update/get task
 router
   .route("/:taskId")
   .get(auth, _getTask)
-  .patch(auth,validate(updateTask), _updateTask);
+  .patch(auth, validate(updateTask), _updateTask);
 
 //export authRoute
 export default router;
--- src/routes/v1/users.ts
+++ src/routes/v1/users.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import validate from "../../middlewares/validate";
 import multer from 'multer';
 import { createUser, getUsers, getUser, updateUser, } from "../../validations/user";
+import { auth,extractUserId } from "../../middlewares/auth";
 import {
   createUser as _createUser,
   getUsers as _getUsers,
   getUser as _getUser,
   updateUser as _updateUser,
   createFromJson,
+  getUserNotifications
 } from "../../controllers/user";
 const upload = multer({   storage: multer.memoryStorage(),}); 
 const router = Router();
@@ -18,6 +20,8 @@ router
   .get(validate(getUsers), _getUsers);
 // uploading json to create users
 router.post('/uploadUsers',upload.single('users'), createFromJson);
+
+router.get('/:userId/notification',auth,extractUserId, getUserNotifications);
 router
   .route("/:userId")
   .get(validate(getUser), _getUser)
